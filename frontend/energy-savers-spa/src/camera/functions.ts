9051c10cd3b398d459d27624bfab8d1b09e8cb22
Mindaugas Dirgincius <mindaugasdirg@gmail.com>
2022-11-06 06:40:27 +0200

    Add debug infor for video resolutions

--- frontend/energy-savers-spa/src/camera/functions.ts
+++ frontend/energy-savers-spa/src/camera/functions.ts
@@ -15,10 +15,15 @@ export const savePicture = (videoFeed: HTMLVideoElement, canvas: HTMLCanvasEleme
   let realHeight = videoFeed.videoHeight / (videoFeed.videoWidth / width);
 
   if (isNaN(realHeight)) {
+    console.log("Couldn't get real video height");
     realHeight = width / (4 / 3);
   }
 
+  console.log(`video resolution: height: ${realHeight}; width: ${width}`);
+  console.log(`canvas resolution: height: ${canvas.height}; width: ${canvas.width}`);
+
   const context = canvas.getContext("2d");
+  console.log(`video resolution: height: ${realHeight}; width: ${width}`);
   context?.drawImage(videoFeed, 0, 0, width, realHeight);
   const imageData = canvas.toDataURL("image/png");
   return { imageData, height: realHeight, width };
